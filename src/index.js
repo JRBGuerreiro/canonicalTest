5a2600028c77d4473f0042583c2b3cc948581d22
Jorge Guerreiro <crcguerreiro@gmail.com>
2021-05-18 23:10:32 +0100

    last changes

--- src/index.js
+++ src/index.js
@@ -32,16 +32,16 @@ function structureHtmlFromData(data) {
         //start populating the DOM
         section.insertAdjacentHTML("afterbegin", 
         `
-        <div class="col-4 u-equal-height">
-            <div class="p-card--highlighted">
-                <p class="p-text--x-small-capitalised u-align--left">${postTag}</p>
-                <hr class="u-sv1">
+        <div class="col-4 u-equal-height p-card--highlighted u-no-margin--bottom u-no-padding" style="flex-direction:column">
+            <header class="card--header">
+                <h5 class="p-text--x-small-capitalised u-align--left u-no-margin--bottom">${postTag}</h5>
+            </header>
+            <div class="card--content">
                 <img class="u-image-position--top" src="${featured_media}" alt="card image"/>
                 <h3 class="p-card__title u-align--left" style="font-weight: 300"><a href=${link}>${title.rendered}</a></h3>
-                <p class="p-heading--6 u-align--left">By <a href="${url}">${name}</a> on ${publishedDate.getUTCDate()} ${month} ${publishedDate.getFullYear()}</p>   
-                <hr class="u-sv1">
-                <p class="u-align--left">${category.slice(0, -1)}</p>
+                <p class="p-heading--6 u-align--left">By <a href="${url}">${name}</a> on ${publishedDate.getUTCDate()} ${month} ${publishedDate.getFullYear()}</p>
             </div>
+            <p class="card--footer u-align--left">${category.slice(0, -1)}</p>
         </div>
         `
         )
